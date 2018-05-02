34364a24ed2dbd0d56480851205653bd3e5ec729
Leo Hussain <leo.94@hotmail.co.uk>
2018-05-02 15:14:58 -0400

    Step 1

--- httpServer.js
+++ httpServer.js
@@ -1,26 +1,20 @@
 var express = require('express');
-
+var http = require('http');
 var path = require("path");
-
 var app = express();
-
+var httpServer = http.createServer(app); //create the server
 var fs = require('fs');
-
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
-
-  extended: true
-
+	extended: true
 }));
 
+httpServer.listen(4480);
+
 app.get('/', function (req, res) {
-
-  // run some server-side code
-
-console.log('the server has received a request'); 
-
-res.send('Hello World');
-
+	// run some server-side code
+	console.log('the server has received a request'); 
+	res.send('Hello World');
 });
 
