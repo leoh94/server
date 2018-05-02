f7824f59cef0c35c6678a85fff800893fbd5993c
Leo Hussain <leo.94@hotmail.co.uk>
2018-05-02 15:44:43 -0400

    server.js functionality

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
 
-app.get('/', function (req, res) {
+app.get('/test.html', function (req, res) {
 	//run some server-side coe
-	console.log('the server has received a request');
-	res.send('Hello World from HTTPS');
+	console.log('test.html requested');
+	res.send(__dirname + '/test.html');
 });
 
